6aabbff390712817dae0244f9515dd93d067a47d
AndriyPapirnyk <papirnykandriy@gmail.com>
2024-06-23 17:18:42 +0300

    creating sidebar routing

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        imgUrl: '/icons/Home.svg',
+        route: '/',
+    },
+    {
+        label: 'Upcoming',
+        imgUrl: '/icons/upcoming.svg',
+        route: '/upcoming',
+    },
+    {
+        label: 'Previos',
+        imgUrl: '/icons/previos.svg',
+        route: '/previos',
+    },
+    {
+        label: 'Recordings',
+        imgUrl: '/icons/Video.svg',
+        route: '/recordings',
+    },
+    {
+        label: 'Personal Room',
+        imgUrl: '/icons/add-personal.svg',
+        route: '/personal-room',
+    },
+]
